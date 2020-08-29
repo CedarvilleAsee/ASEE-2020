56103bb874293072ad52887f16d3c870e94c1f39
Tintinabulum <jordanhopkins@cedarville.edu>
2020-08-29 11:21:22 -0400

    Update lineFollowing.h

--- Retreval_v5_RedGoal/lineFollowing.h
+++ Retreval_v5_RedGoal/lineFollowing.h
@@ -75,4 +75,20 @@ bool lineFollow(int ts, int strictness, int cen1 = 4, int cen2 = 3) {
   return false;
 }
 
+//if (favorRight) cen=3 else cen=4 (that is, for getting the favored line to center
+void favorLineFollow(int ts,int strictness,bool favorRight, int cen=3){
+  //Sees no lines, go straight
+  if(amountSeen==0){
+    writeToWheels(ts,ts);
+    return;
+  }
+  //Diff is the difference between the central index (cen) and the index of the line it's following
+  int diff;
+  if(favorRight) diff=firstLineIndex-cen;
+  else diff=lastLineIndex-cen; 
+  int rightSpeed = ts - diff*strictness;
+  int leftSpeed = ts + diff*strictness;
+  writeToWheel(leftSpeed,rightSpeed);
+}
+
 #endif
