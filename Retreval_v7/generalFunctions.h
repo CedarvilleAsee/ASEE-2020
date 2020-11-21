2e0e3d1d07135c212a3f1993b475e54b072b9bae
Tintinabulum <60529494+Tintinabulum@users.noreply.github.com>
2020-11-21 12:18:08 -0500

    Create generalFunctions.h

---
+++ Retreval_v7/generalFunctions.h
@@ -0,0 +1,17 @@
+void closeClaw(){
+  if(clawOpen){
+    clawOpen = false;
+    //Code to close the holder
+    clawMotor.write(CLOSED_ANGLE);
+  }
+}
+void openClaw(){
+  if(!clawOpen){
+    clawOpen = true;
+    //Code to open the holder
+    clawMotor.write(OPEN_ANGLE);
+  }
+}
+void Debug(){
+ 
+}
