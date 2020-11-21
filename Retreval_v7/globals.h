9ea0e4f85d5e5604a4b2db2b7643ad78473d08b5
Tintinabulum <60529494+Tintinabulum@users.noreply.github.com>
2020-11-21 12:18:11 -0500

    Create globals.h

---
+++ Retreval_v7/globals.h
@@ -0,0 +1,20 @@
+#ifndef GLOBALS
+#define GLOBALS
+
+//Line following
+int sensors[8]     = {0};
+int firstLineIndex = -1;
+int lastLineIndex  = -1;
+int amountSeen     = 0;
+float TimeInState = 0;
+int CurrentState = 1;
+
+//delta time global
+unsigned long CurrentTime = 0.0;
+
+Servo clawMotor;
+bool clawOpen = false;
+
+PT6961 display(DIN, CLOCK, CS);
+
+#endif
