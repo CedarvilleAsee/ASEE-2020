c222e5271c6a3540d0fbacea3cf6c52ec88175e1
Tintinabulum <60529494+Tintinabulum@users.noreply.github.com>
2020-11-21 12:18:05 -0500

    Create constants.h

---
+++ Retreval_v7/constants.h
@@ -0,0 +1,28 @@
+#ifndef CONSTANTS
+
+#define CONSTANTS
+
+//Puck Constants
+const int PUCK_RECIEVED = 300;
+//const int TIME_IN_HOLDER = 333;
+const int OPEN_ANGLE = 120;
+const int CLOSED_ANGLE = 60;
+
+
+//speed constants
+const int TURN_SPEED = 65;
+const int FULL_SPEED = 120;
+const int MAX_SPEED = 80;
+const int LINE_STRICTNESS = 20;
+
+// Dasch constants
+const bool WHEEL_FORWARDS   = true;
+
+const int RIGHT_CLAW_OPEN = 95;
+const int RIGHT_CLAW_CLOSE = 47;//47
+
+const int LEFT_CLAW_OPEN = 60;//85 open
+const int LEFT_CLAW_CLOSE = 110;//110;
+
+
+#endif
