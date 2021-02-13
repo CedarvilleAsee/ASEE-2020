fd6cd774214f538f91503b9f92e09d9be2380161
Cook-48427 <60040038+Cook-48427@users.noreply.github.com>
2021-02-13 12:00:39 -0500

    Update globals.h

--- Retreval_v7/globals.h
+++ Retreval_v7/globals.h
@@ -6,9 +6,10 @@ int sensors[8]     = {0};
 int firstLineIndex = -1;
 int lastLineIndex  = -1;
 int amountSeen     = 0;
-float TimeInState = 0;
+float TimeInState  = 0;
 float TimeInLastState = 0;
 int CurrentState = 1;
+int substateFlag = 0;
 
 //delta time global
 unsigned long CurrentTime = 0.0;
